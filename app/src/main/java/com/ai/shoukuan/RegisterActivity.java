9b3c4f9a9ce7b0fd1117a8c1292b827be4a3b6e8
wumh <wumh@xmxycx.com>
2019-04-24 18:33:32 +0800

    测试账号密码

--- app/src/main/java/com/ai/shoukuan/RegisterActivity.java
+++ app/src/main/java/com/ai/shoukuan/RegisterActivity.java
@@ -1,9 +1,14 @@
 package com.ai.shoukuan;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 
 public class RegisterActivity extends Activity {
 
-
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 }
 
